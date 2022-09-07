e7062644f216df7b27d9e419f70ee2072053d14b
Batykov Gleb <gleb.batykov@gmail.com>
2022-09-07 16:45:16 +0300

    Updates test

--- test/numbers_to_words_russian_test.dart
+++ test/numbers_to_words_russian_test.dart
@@ -1,11 +1,11 @@
-import 'package:numbers_to_words_russian/numbers_to_words.dart';
+import 'package:numbers_to_words_russian/numbers_to_words_russian.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('Convert zero', () {
     var number = 0;
 
-    var result = NumberToWordsRussian.convert(number);
+    var result = NumbersToWordsRussian.convert(number);
 
     expect(result, 'ноль');
   });
@@ -13,7 +13,7 @@ void main() {
   test('Convert less twenty', () {
     var number = 11;
 
-    var result = NumberToWordsRussian.convert(number);
+    var result = NumbersToWordsRussian.convert(number);
 
     expect(result, 'одиннадцать');
   });
@@ -21,7 +21,7 @@ void main() {
   test('Convert less hundred', () {
     var number = 88;
 
-    var result = NumberToWordsRussian.convert(number);
+    var result = NumbersToWordsRussian.convert(number);
 
     expect(result, 'восемьдесят восемь');
   });
@@ -29,7 +29,7 @@ void main() {
   test('Convert less thousand', () {
     var number = 867;
 
-    var result = NumberToWordsRussian.convert(number);
+    var result = NumbersToWordsRussian.convert(number);
 
     expect(result, 'восемьсот шестьдесят семь');
   });
@@ -37,7 +37,7 @@ void main() {
   test('Convert less million', () {
     var number = 127835;
 
-    var result = NumberToWordsRussian.convert(number);
+    var result = NumbersToWordsRussian.convert(number);
 
     expect(result, 'сто двадцать семь тысяч восемьсот тридцать пять');
   });
@@ -45,7 +45,7 @@ void main() {
   test('Convert less billion', () {
     var number = 5000125;
 
-    var result = NumberToWordsRussian.convert(number);
+    var result = NumbersToWordsRussian.convert(number);
 
     expect(result, 'пять миллионов сто двадцать пять');
   });
@@ -53,7 +53,7 @@ void main() {
   test('Convert less trillion', () {
     var number = 25125888005;
 
-    var result = NumberToWordsRussian.convert(number);
+    var result = NumbersToWordsRussian.convert(number);
 
     expect(result,
         'двадцать пять миллиардов сто двадцать пять миллионов восемьсот восемьдесят восемь тысяч пять');
